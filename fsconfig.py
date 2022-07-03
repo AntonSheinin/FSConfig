6c4046caa58a5a6894fceb3a68b7cb8466a840b5
AntonSheinin <anton.sheinin@gmail.com>
2022-07-03 13:51:43 +0300

    fix

--- fsconfig.py
+++ fsconfig.py
@@ -23,10 +23,9 @@ menu_links = {'main-menu' : 'MainMenu',
 redis_сlient = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_streams_via_api(session):
-    auth = HTTPBasicAuth('flussonic', '2V3kTTJ4b2AKW9Ls')
     api_method = 'streams'
     url = 'http://193.176.179.222:8085/flussonic/api/v3/'
-    r = requests.get(''.join((url, api_method)), auth)
+    r = requests.get(''.join((url, api_method)), auth = HTTPBasicAuth('flussonic', '2V3kTTJ4b2AKW9Ls'))
 
     print (r.json())
 
