11a849d08c6bcf0a1353b20c02892b95b2ae7e52
AntonSheinin <anton.sheinin@gmail.com>
2022-07-07 13:55:24 +0300

    fix

--- fsconfig.py
+++ fsconfig.py
@@ -24,7 +24,7 @@ menu_links = {'main-menu' : 'MainMenu',
 redis_сlient = redis.Redis(host='localhost', port=6379, db=0)
 
 def TestPutApi():
-    payload = {'name' : 'test1'}
+    payload = {"name" : "test1"}
     api_call('streams/test', 'PUT', payload, 'flussonic', '2V3kTTJ4b2AKW9Ls')
 
 def api_call(query, request_method, json_payload, username, password):
