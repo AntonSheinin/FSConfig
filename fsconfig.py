b954c949ab20f7bd34420f62b823ce41b3aaa80a
AntonSheinin <anton.sheinin@gmail.com>
2022-07-19 12:58:07 +0300

    fix

--- fsconfig.py
+++ fsconfig.py
@@ -131,7 +131,7 @@ def dvr_settings(config, choosen_channels, session):
             if dvr_limit == 0:
                 del stream['dvr']
 
-            redis_client.json.arrappend('changed_channels' + session, Path.root_path(), {stream['name'] : 'dvr'})
+            redis_client.json().arrappend('changed_channels' + session, Path.root_path(), {stream['name'] : 'dvr'})
 
     return template('templates/dvr_complete.tpl'), config
 
