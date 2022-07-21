de30c3fe9c07f170ffe39f1975ce7372c368b027
AntonSheinin <anton.sheinin@gmail.com>
2022-07-21 23:15:53 +0300

    fix

--- fsconfig.py
+++ fsconfig.py
@@ -97,7 +97,7 @@ def changed_channels_list_update(session, channel_name, channel_entity):
     print(channel_entity)
 
     redis_client.json().set('changed_channels' + session, '.' + str(changed_channels['count']), {'name' : channel_name, 'entity' : channel_entity})
-    #redis_client.json().set('changed_channels' + session, '.', {'count' : changed_channels['count'] + 1})
+    redis_client.json().set('changed_channels' + session, '.count', changed_channels['count'] + 1)
 
 def choose_channels(session):
 
