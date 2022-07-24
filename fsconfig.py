e78b89b95abd6fe2bba1f78dc54f8f92349267ea
AntonSheinin <anton.sheinin@gmail.com>
2022-07-24 11:42:39 +0300

    fix

--- fsconfig.py
+++ fsconfig.py
@@ -93,12 +93,12 @@ def changed_channels_list_update(session, channel_name, channel_entity):
     changed_channels = {}
 
     if not redis_client.exists('changed_channels' + session):
-        redis_client.json().set('changed_channels' + session,'.', {'count' : '0', 'streams' : {}})
+        redis_client.json().set('changed_channels' + session,'.', {'count' : '0', 'streams' : []})
 
     count = int(redis_client.json().get('changed_channels' + session, '.count'))
     changed_channels.update(redis_client.json().get('changed_channels'+ session, '.'))
 
-    changed_channels.update({'streams' : {'name' : channel_name, 'entity' : channel_entity}})
+    changed_channels['streams'][count] = {'name' : channel_name, 'entity' : channel_entity}
 
     count += 1
     changed_channels.update({'count' : count})
